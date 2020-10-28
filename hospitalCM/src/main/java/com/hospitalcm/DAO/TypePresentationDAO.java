c97518fabd5e12e73c680d2b5599a87d295f5dc2
Denzil Barrios <dbarriosb1@miumg.edu.gt>
2020-10-28 17:22:30 -0600

    Se agrego la api TypePresentation

---
+++ hospitalCM/src/main/java/com/hospitalcm/DAO/TypePresentationDAO.java
@@ -0,0 +1,183 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hospitalcm.DAO;
+
+import com.hospitalcm.conexion.Conexion;
+import com.hospitalcm.model.TypePresentationModel;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author Denzil Barrios
+ * GoDevs Team-2,020
+ */
+
+
+public class TypePresentationDAO {
+      //Conexión
+    Connection connection;
+    Conexion con = new Conexion();
+    
+    ResultSet rs;
+    int resultado;
+ 
+    //Declaración de variables de objeto
+    int tp_id;
+    String tp_name;
+    String tp_description;
+    
+    //Declaración de Consultas
+    //Declaración de consultas a DB
+    String selectALL = "Select * from gearsgtc_java_hospital.hl_TypePresentation";
+    String selectByID = "Select * from gearsgtc_java_hospital.hl_TypePresentation WHERE tp_id = ";
+    String deleteByID = "Delete From gearsgtc_java_hospital.hl_TypePresentation WHERE tp_id = ";
+    String UPDATE = "UPDATE gearsgtc_java_hospital.hl_TypePresentation SET tp_name = (?), tp_description = (?) WHERE tp_id = ";
+    String INSERT = "Insert into gearsgtc_java_hospital.hl_TypePresentation  VALUES (NULL, ?, ?)";
+    
+    //MÉTODOS CRUD
+    //AGREGAR
+    public boolean agregarDAO(TypePresentationModel objUse){
+        boolean res;
+        
+        try{
+            String sql = INSERT;
+            connection = con.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+            //statement.setInt(1, objUse.getMdDetails_id());
+            statement.setInt(1, objUse.getTp_id());
+            statement.setString(2, objUse.getTp_name());
+            statement.setString(4, objUse.getTp_description());
+            statement.execute();
+            connection.close();
+            res = true;
+            
+        }catch(SQLException e){
+            e.getMessage();
+            res = false;
+           return res;
+        }
+        
+        return res;
+    }
+    
+    //OBTENER TODOS LOS REGISTROS
+    public  List<TypePresentationModel> obtenerTodosDAO(){
+        boolean res;
+        List<TypePresentationModel> listaTodos = new ArrayList<TypePresentationModel>();
+        
+        try{
+            String sql = selectALL;
+            connection = con.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.execute();
+            rs = statement.executeQuery(sql);
+            
+            res = true;
+            
+            if(res){
+                while(rs.next()){
+                    tp_id = rs.getInt("tp_id");
+                    tp_name = rs.getString("tp_name");
+                    tp_description = rs.getString("tp_description");
+                    TypePresentationModel objUse = new TypePresentationModel(tp_id, tp_name, tp_description);
+                    listaTodos.add(objUse);
+                }
+            }
+            connection.close();
+        }catch(SQLException e){
+            e.getMessage();
+           return listaTodos;
+        }
+        
+        return listaTodos;
+    }
+    
+    //OBTENER UNA REGISTRO
+     public  TypePresentationModel obtenerDAO(int idBuscar){
+        boolean res;
+        TypePresentationModel objUse;
+        
+        try{
+            String sql = selectByID + Integer.toString(idBuscar);
+            connection = con.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.execute();
+            rs = statement.executeQuery(sql);
+            
+            res = true;
+            
+            if(res){
+                while(rs.next()){
+                    tp_id = rs.getInt("tp_id");
+                    tp_name = rs.getString("tp_name");
+                    tp_description = rs.getString("tp_description");
+                }
+            }
+            connection.close();
+        }catch(SQLException e){
+            e.getMessage();
+           return null;
+        }
+        objUse = new TypePresentationModel(tp_id, tp_name, tp_description);
+        return objUse;
+    }
+     
+     //ELIMINAR
+     public boolean eliminarDAO(int idBuscar){
+        boolean res;
+        
+        try{
+            String sql =  deleteByID + Integer.toString(idBuscar);
+            connection = con.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+            
+            statement.execute();
+            connection.close();
+            res = true;
+            
+        }catch(SQLException e){
+            e.getMessage();
+            res = false;
+           return res;
+        }
+        
+        return res;
+    }
+     
+     
+    //ACTUALIZAR
+     public boolean actualizarDAO(TypePresentationModel objUse){
+        boolean res;
+        
+        try{
+            String sql = UPDATE;
+            connection = con.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+            //statement.setInt(1, objUse.getMdDetails_id());
+            statement.setInt(1, objUse.getTp_id());
+            statement.setString(2, objUse.getTp_name());
+            statement.setString(4, objUse.getTp_description());
+            statement.execute();
+            connection.close();
+            res = true;
+            
+        }catch(SQLException e){
+            e.getMessage();
+            res = false;
+           return res;
+        }
+        
+        return res;
+    }
+
+}
---
+++ hospitalCM/src/main/java/com/hospitalcm/model/TypePresentationModel.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hospitalcm.model;
+
+/**
+ *
+ * @author Denzil Barrios
+ * GoDevs Team-2,020
+ */
+
+public class TypePresentationModel {
+    int tp_id;
+    String tp_name;
+    String tp_description;
+
+    public TypePresentationModel() {
+    }
+
+    public TypePresentationModel(int tp_id, String tp_name, String tp_description) {
+        this.tp_id = tp_id;
+        this.tp_name = tp_name;
+        this.tp_description = tp_description;
+    }
+
+    public int getTp_id() {
+        return this.tp_id;
+    }
+
+    public void setTp_id(int tp_id) {
+        this.tp_id = tp_id;
+    }
+
+    public String getTp_name() {
+        return this.tp_name;
+    }
+
+    public void setTp_name(String tp_name) {
+        this.tp_name = tp_name;
+    }
+
+    public String getTp_description() {
+        return this.tp_description;
+    }
+
+    public void setTp_description(String tp_description) {
+        this.tp_description = tp_description;
+    }
+       
+}
