a96ccc3dbbc8d57b4249adef079a6acf058ffcea
Cristian Coronado <ccoronadog2@miumg.edu.gt>
2020-10-30 20:11:05 -0600

    Api Patients con todos sus métodos

--- hospitalCM/src/main/java/com/hospitalcm/DAO/patientsDAO.java
+++ hospitalCM/src/main/java/com/hospitalcm/DAO/patientsDAO.java
@@ -42,93 +42,153 @@ public class patientsDAO {
     String selectALL = "Select * from gearsgtc_java_hospital.hl_Patients";
     String selectByID = "Select * from gearsgtc_java_hospital.hl_Patients where patient_id = ";
     String deleteByID = "Delete From gearsgtc_java_hospital.hl_Patients where patient_id = ";
-    String updateByID = "";
+    String UPDATE = "UPDATE gearsgtc_java_hospital.hl_Patients SET patient_startdate = (?), patient_username = (?), patient_password = (?), patient_stopdate = (?), patient_active = (?), user_id = (?), patient_register = (?) where patient_id = ";
     String INSERT = "Insert into gearsgtc_java_hospital.hl_Patients VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)";
     
     //Creación de métodos
     //ADD/AGREGAR, Recibe un objeto de tipo ROL
-   public boolean addPatient(Patients_Model objPatient){
-       //Variable de resultado que se retornará
-       boolean res;
+    public boolean addPatient(Patients_Model objPatient){
        
-       try{
-           //ASignamos la consulta a la variable sql
-           String sql = INSERT;
-           //Obtenemos la conexión a la DB
-           connection = con.getConnection();
-           //Preparamos la consulta
-           PreparedStatement statement = connection.prepareStatement(sql);
-           
-           //ASignamos los valores a la consulta INSERT
-           statement.setDate(1, objPatient.getPatient_startdate());
-           statement.setString(2, objPatient.getPatient_username());
-           statement.setString(3, objPatient.getPatient_password());
-           statement.setDate(4, objPatient.getPatient_stopdate());
-           statement.setBoolean(5, objPatient.isPatient_active());
-           statement.setInt(6, objPatient.getUser_id());
-           statement.setDate(7, objPatient.getPatient_register());
-           
-           //Ejecutamos la consulta
-           statement.execute();
-           //Cerramos la conexión
-           connection.close();
-           //ASignamos la respuesta como true
-           res = true;
-           return res;
-       }catch(SQLException e){
-           e.getMessage();
-           res = false;
-           return res;
-       }
-   }
+        try{
+            //ASignamos la consulta a la variable sql
+            String sql = INSERT;
+            //Obtenemos la conexión a la DB
+            connection = con.getConnection();
+            //Preparamos la consulta
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            //ASignamos los valores a la consulta INSERT
+            statement.setDate(1, objPatient.getPatient_startdate());
+            statement.setString(2, objPatient.getPatient_username());
+            statement.setString(3, objPatient.getPatient_password());
+            statement.setDate(4, objPatient.getPatient_stopdate());
+            statement.setBoolean(5, objPatient.isPatient_active());
+            statement.setInt(6, objPatient.getUser_id());
+            statement.setDate(7, objPatient.getPatient_register());
+
+            //Ejecutamos la consulta
+            statement.execute();
+            //Cerramos la conexión
+            connection.close();
+            //ASignamos la respuesta como true
+            res = true;
+            return res;
+        }catch(SQLException e){
+            e.getMessage();
+            res = false;
+            return res;
+        }
+    }
     
+   //OBTENER TODOS LOS REGISTROS
+    public List<Patients_Model> getPatients(){
+        //Creación de la lista que se devolverá como respuesta
+        List<Patients_Model> listaPatients = new ArrayList<Patients_Model>();
+
+        try{
+            connection = con.getConnection();
+            PreparedStatement statement = connection.prepareStatement(selectALL);
+            statement.execute();
+            rs = statement.executeQuery();
+
+            res = true;
+            if(res){
+                while(rs.next()){
+                    patient_id = rs.getInt("patient_id");
+                    Patient_startdate = rs.getDate("patient_startdate");
+                    Patient_username = rs.getString("patient_username");
+                    Patient_password = rs.getString("patient_password");
+                    Patient_stopdate = rs.getDate("patient_stopdate");
+                    Patient_active = rs.getBoolean("patient_active");
+                    user_id = rs.getInt("user_id");
+                    patient_register = rs.getDate("patient_register");
+
+                    Patients_Model objPatients = new Patients_Model( patient_id,  Patient_startdate, Patient_username,  Patient_password ,  Patient_stopdate,Patient_active, user_id, patient_register);
+
+                    listaPatients.add(objPatients);
+                }
+            }
+
+            connection.close();
+            return listaPatients;
+        }catch(SQLException e){
+            return listaPatients;
+        }
+    }
    
-     public List<Patients_Model> getPatients(){
-       //Creación de la lista que se devolverá como respuesta
-       List<Patients_Model> listaPatients = new ArrayList<Patients_Model>();
-       
-       try{
-           connection = con.getConnection();
-           PreparedStatement statement = connection.prepareStatement(selectALL);
-           statement.execute();
-           rs = statement.executeQuery();
-           
-           res = true;
-           if(res){
-               while(rs.next()){
-                   patient_id = rs.getInt("patient_id");
-                   Patient_startdate = rs.getDate("patient_startdate");
-                   Patient_username = rs.getString("patient_username");
-                   Patient_password = rs.getString("patient_password");
-                   Patient_stopdate = rs.getDate("patient_stopdate");
-                   Patient_active = rs.getBoolean("patient_active");
-                   user_id = rs.getInt("user_id");
-                   patient_register = rs.getDate("patient_register");
-                           
-                   Patients_Model objPatients = new Patients_Model( patient_id,  Patient_startdate, Patient_username,  Patient_password ,  Patient_stopdate,Patient_active, user_id, patient_register);
-                   
-                   listaPatients.add(objPatients);
-               }
-           }
-           
-           connection.close();
-           return listaPatients;
-       }catch(SQLException e){
-           return listaPatients;
-       }
-   }
+    //OBTENER UN REGISTRO
+    public Patients_Model getPatient(int idBuscar){
+        Patients_Model objPatients;
+        try{
+            connection = con.getConnection();
+            PreparedStatement statement = connection.prepareStatement(selectALL);
+            statement.execute();
+            rs = statement.executeQuery();
+
+            res = true;
+            if(res){
+                while(rs.next()){
+                    patient_id = rs.getInt("patient_id");
+                    Patient_startdate = rs.getDate("patient_startdate");
+                    Patient_username = rs.getString("patient_username");
+                    Patient_password = rs.getString("patient_password");
+                    Patient_stopdate = rs.getDate("patient_stopdate");
+                    Patient_active = rs.getBoolean("patient_active");
+                    user_id = rs.getInt("user_id");
+                    patient_register = rs.getDate("patient_register");
+                }
+            }
+
+            connection.close();
+            objPatients = new Patients_Model( patient_id,  Patient_startdate, Patient_username,  Patient_password ,  Patient_stopdate,Patient_active, user_id, patient_register);
+            return objPatients;
+        }catch(SQLException e){
+            e.getMessage();
+            return null;
+        }
+    }
    
-   public void getPatient(){
-       
-   }
-   
-   public void deletePatient(){
-       
-   }
-   
-   public void updatePatient(){
-       
-   }
-    
-   
+    //ELIMINAR UN REGISTRO
+    public boolean deletePatient(int idBuscar){
+        try{
+            String sql =  deleteByID + Integer.toString(idBuscar);
+            connection = con.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+            
+            statement.execute();
+            connection.close();
+            res = true;
+            return res;
+        }catch(SQLException e){
+            e.getMessage();
+            res = false;
+           return res;
+        }
+    }
+
+    //ACTUALIZAR UN REGISTRO
+    public boolean updatePatient(Patients_Model objPatient){
+        try{
+            String sql = UPDATE + objPatient.getPatient_id();
+            connection = con.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+            
+            statement.setDate(1, objPatient.getPatient_startdate());
+            statement.setString(2, objPatient.getPatient_username());
+            statement.setString(3, objPatient.getPatient_password());
+            statement.setDate(4, objPatient.getPatient_stopdate());
+            statement.setBoolean(5, objPatient.isPatient_active());
+            statement.setInt(6, objPatient.getUser_id());
+            statement.setDate(7, objPatient.getPatient_register());
+            statement.execute();
+            connection.close();
+            res = true;
+            return res;
+        }catch(SQLException e){
+            e.getMessage();
+            res = false;
+           return res;
+        }
+    }
+     
 }
--- hospitalCM/src/main/java/com/hospitalcm/api/patientsApi.java
+++ hospitalCM/src/main/java/com/hospitalcm/api/patientsApi.java
@@ -32,6 +32,7 @@ public class patientsApi {
        //Instanciamos el objeto DAO para poder acceder a sus métodos
     patientsDAO objPatients = new patientsDAO();
     
+    boolean res;
     /*Método POST: Utilizado para insertar un rol en la tabla de
                    base de datos correspondiente
     */
@@ -39,12 +40,12 @@ public class patientsApi {
     
     public Response addPatients(Patients_Model patients){
         //Guarda el retorno de la operación del DAO
-        boolean result = objPatients.addPatient(patients);
+        res = objPatients.addPatient(patients);
          
         
         
         //Valida que la creación fue exitosa
-        if(result){
+        if(res){
             //eEtorna una respuesta de tipo Json si este fue creado exitosamente
             return Response.status(Response.Status.CREATED).build();
         }else{
@@ -52,6 +53,10 @@ public class patientsApi {
             return Response.status(400, "Ocurrió un error al registrar el paciente").build();
         }
     }
+    
+    /*Método GET: Utilizado para SELECCIONAR todos registros en la tabla de 
+                base de datos correspondiente
+    */
     @GET
     public List<Patients_Model> getPatients(){
          List<Patients_Model> listaPatients = new ArrayList<Patients_Model>();
@@ -59,4 +64,47 @@ public class patientsApi {
          return listaPatients;
     }
     
+    /*Método GET: Utilizado para SELECCIONAR  un registro en la tabla 
+                de base de datos correspondiente por medio de su ID
+    */
+    @GET
+    @Path("/{id}")
+    public Response getPatient(@PathParam("id") int id){
+        Patients_Model objPatient = objPatients.getPatient(id);
+        
+        if(objPatient.getPatient_id() != 0){
+            return Response.ok(objPatient).build();
+        }else if(objPatient.getPatient_id() == 0){
+            return Response.status(200, "Registro no encontrado en base de datos").build();
+        }else{
+            return Response.status(500, "Ocurrió un error al consultar el registro").build();
+        }
+    }
+    
+    /*Método DELETE: Utilizado para ELIMINAR un registro en la tabla 
+                    de base de datos correspondiente
+    */
+    @DELETE
+    @Path("/{id}")
+    public Response deletePatient(@PathParam("id") int id){
+        res = objPatients.deletePatient(id);
+        if(res){
+            return Response.status(200, "Registro eliminado correctamente").build();
+        }else{
+            return Response.status(500, "Ocurrió un error al eliminar el registro").build();
+        }
+    }
+    
+    /*Método PUT: Utilizado para ACTUALIZAR un registro en la tabla 
+                    de base de datos correspondiente
+    */
+    @PUT
+    public Response updatePatient(Patients_Model objPatient){
+        res = objPatients.updatePatient(objPatient);
+        if(res){
+            return Response.ok(objPatient).build();
+        }else{
+            return Response.status(500, "Ocurrió un error al actualizar el usuario, intente más tarde").build();
+        }
+    }
 }
