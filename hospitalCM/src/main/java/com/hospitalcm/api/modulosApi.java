543a4c64588f04a6b938f1d8448b60feb5c0b08e
Cristian Coronado <ccoronadog2@miumg.edu.gt>
2020-11-09 19:14:56 -0600

    Cambio de mensajes en API Modulos

--- hospitalCM/src/main/java/com/hospitalcm/api/modulosApi.java
+++ hospitalCM/src/main/java/com/hospitalcm/api/modulosApi.java
@@ -45,10 +45,10 @@ public class modulosApi {
         //VAlida que la creación fue exitosa
         if(res){
             //REtorna una respuesta de tipo Json si este fue creado exitosamente
-            return Response.status(Response.Status.CREATED).build();
+            return Response.ok("Registro creado correctamente").build();
         }else{
             //Retorna una respuesta de tipo Json con un mensaje de error
-            return Response.status(400, "Ocurrió un error al registrar el modulo").build();
+            return Response.ok("Ocurrió un error al agregar el registro ").build();
         }
     }
     
@@ -73,9 +73,9 @@ public class modulosApi {
         if(module.getModule_id() != 0){
             return Response.ok(module).build();
         }else if(module.getModule_id() == 0){
-            return Response.status(200, "Registro no encontrado en base de datos").build();
+            return Response.ok("Registro no encontrado en base de datos").build();
         }else{
-            return Response.status(500, "Ocurrió un error al consultar el registro").build();
+            return Response.ok("Ocurrió un error al consultar el registro").build();
         }
     }
     
@@ -87,9 +87,9 @@ public class modulosApi {
     public Response deleteModule(@PathParam("id") int id){
         res = objModule.deleteModule(id);
         if(res){
-            return Response.status(200, "Registro eliminado correctamente").build();
+            return Response.ok("Registro eliminado correctamente").build();
         }else{
-            return Response.status(500, "Ocurrió un error al eliminar el registro").build();
+            return Response.ok("Ocurrió un error al eliminar el registro").build();
         }
     }
     
@@ -102,7 +102,7 @@ public class modulosApi {
         if(res){
             return Response.ok(module).build();
         }else{
-            return Response.status(500, "Ocurrió un error al actualizar el usuario, intente más tarde").build();
+            return Response.ok("Ocurrió un error al actualizar el registro, intente más tarde").build();
         }
     }
 }
