ec18740806bf4a2a6746359cae57a7c28d5f9900
Cristian Coronado <ccoronadog2@miumg.edu.gt>
2020-11-09 19:03:11 -0600

    Cambio de mensajes en API Medicine

--- hospitalCM/src/main/java/com/hospitalcm/api/medicineApi.java
+++ hospitalCM/src/main/java/com/hospitalcm/api/medicineApi.java
@@ -48,9 +48,9 @@ public class medicineApi {
         
         boolean agregado = objUse.agregarDAO(registro);  
         if (agregado){
-            return Response.status(Response.Status.CREATED).build();
+            return Response.ok("Registro creado correctamente").build();
         }else{
-            return Response.status(404, "Ocurrió un error al insertar el registro").build();
+            return Response.ok("Ocurrió un error al agregar el registro ").build();
         }
     }
     
@@ -70,14 +70,15 @@ public class medicineApi {
     */
     @GET
     @Path("/{id}")
-    public MedicineModel registro(@PathParam("id") int id){
-        MedicineModel objRegistro = new MedicineModel();
-        objRegistro = objUse.obtenerDAO(id);
+    public Response registro(@PathParam("id") int id){
+        MedicineModel objRegistro = objUse.obtenerDAO(id);
         
-        if(objRegistro != null){
-            return objRegistro;
+        if(objRegistro.getMedicine_id() != 0){
+            return Response.ok(objRegistro).build();
+        }else if(objRegistro.getMedicine_id() == 0){
+            return Response.ok("Registro no encontrado en base de datos").build();
         }else{
-            return null;
+            return Response.ok("Ocurrió un error al consultar el registro").build();
         }
     }
     
@@ -90,9 +91,9 @@ public class medicineApi {
         boolean eliminado = objUse.eliminarDAO(id);
         
         if(eliminado){
-            return Response.status(200, "¡Eliminado Correctamente!").build();
+             return Response.ok("Registro eliminado correctamente").build();
         }else{
-            return Response.status(500, "Ocurrió un error al tratar de eliminar el registro, intente más tarde").build();   
+            return Response.ok("Ocurrió un error al eliminar el registro").build();
         }
     }
     
@@ -104,9 +105,9 @@ public class medicineApi {
         
         boolean agregado = objUse.actualizarDAO(registro);  
         if (agregado){
-            return Response.status(200, "¡Registro actualizado exitosamente!").build();
+            return Response.ok(registro).build();
         }else{
-            return Response.status(404, "Ocurrió un error al actualizar el registro").build();
+            return Response.ok("Ocurrió un error al actualizar el registro, intente más tarde").build();
         }
         
     }    
